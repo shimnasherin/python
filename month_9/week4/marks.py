4fc5604e97c7c7f21664274f791e9ae8ed1a73d4
shimnasherin <shimnasherin005@gmail.com>
2024-10-07 09:42:45 +0100

    Add files via upload

---
+++ month_9/week4/marks.py
@@ -0,0 +1,10 @@
+a=float(input("enter the marks of A"))
+b=float(input("enter the marks of B"))
+c=float(input("enter the marks of C"))
+d=float(input("enter the marks of D"))
+e=float(input("enter the marks of E"))
+total=a+b+c+d+e
+print("Total",total)
+percentage(total/50)*100
+print("percentage",percentage)
+
